2feda376a5d97ba5e4a2c09d172eea278043b4e8
Ryo <xxxpppd7@gmail.com>
2021-07-20 23:15:10 +0900

    Initial Commit

---
+++ WorkSharing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorkSharing
+//
+//  Created by TERANAKA RYO on 2021/07/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
